00c815256bef9b7d13aa209076596220c92f3b90
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-06-29 10:27:44 +0200

    Divide the skills section into skill bar components

--- src/components/Who/index.js
+++ src/components/Who/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { FaGithub, FaUser } from "react-icons/fa";
+import SkillBox from "./SkillBox";
+import skills from "./skills";
 
 const WhoPage = () => {
    return(
@@ -14,93 +16,19 @@ const WhoPage = () => {
           With over 3 years of experience in front-end and back-end development.
       </div>
       <a href="https://www.github.com/rukundo-kevin" target="blank">
-                    <button className="btn github-btn">
+                  <button className="btn github-btn">
                       <FaGithub />
                     View Github Profile 
                   </button>
-                </a>
-
+      </a>
       </div>
     </div>
-    <div className="social skills">
-        <h2 style={{textAlign: "center"}}>Front-end Skills</h2>
-        HTML5
-        <div className="skill-bar">
-            <div className="bar" id="html"></div>
-        </div>
-        CSS3
-        <div className="skill-bar">
-            <div className="bar" id="css"></div>
-        </div>
-        JS + ES6
-        <div className="skill-bar">
-            <div className="bar" id="js"></div>
-        </div>
-        BOOTSTRAP
-        <div className="skill-bar">
-            <div className="bar" id="bs"></div>
-        </div>
-          REACT
-        <div className="skill-bar">
-            <div className="bar" id="react"></div>
-        </div>
-          SASS
-        <div className="skill-bar">
-            <div className="bar" id="mocha"></div>
-        </div>
-          TAILWIND
-        <div className="skill-bar">
-            <div className="bar" id="express"></div>
-        </div>
-          D3.JS
-        <div className="skill-bar">
-            <div className="bar" id="d3"></div>
-        </div>
-          JQUERY
-        <div className="skill-bar">
-            <div className="bar" id="c++"></div>
-        </div>
-    </div>
-    <div className="social skills">
-                  <h2 style={{textAlign: "center"}}>Back-end Skills</h2>
-        EXPRESSJS
-        <div className="skill-bar">
-            <div className="bar" id="html"></div>
-        </div>
-        NODEJS
-        <div className="skill-bar">
-            <div className="bar" id="css"></div>
-        </div>
-        JS + ES6
-        <div className="skill-bar">
-            <div className="bar" id="js"></div>
-        </div>
-          NEXTJS
-        <div className="skill-bar">
-            <div className="bar" id="bs"></div>
-        </div>
-          PHP7
-        <div className="skill-bar">
-            <div className="bar" id="react"></div>
-        </div>
-          LARAVEL
-        <div className="skill-bar">
-            <div className="bar" id="nodejs"></div>
-        </div>
-          C
-        <div className="skill-bar">
-            <div className="bar" id="d3"></div>
-        </div>
-          TESTING(MOCHA/CHAI)
-        <div className="skill-bar">
-            <div className="bar" id="mocha"></div>
-        </div>
-          C++
-        <div className="skill-bar">
-            <div className="bar" id="c++"></div>
-        </div>
-    </div>
-  </div>
+     {
+      skills.map(({id, skillEnd, skills})=>(
+        <SkillBox key={id} title={skillEnd} skills={skills}/>
+      ))
+     }
+     </div>
    )
 }
 
--- src/components/Why/index.js
+++ src/components/Why/index.js
@@ -10,40 +10,38 @@ import { ReactComponent as SocialMediaSvg } from '../img/socialMedia.svg';
 const WhyPage = () =>{
  return(
     <div className="container" id="why">
-    <div className="why-text"> I Don't  Like
-            <IconContext.Provider value={{ color: "#ee354b", className: "fa-1x" }}>
-                <FaLaptop />
-            </IconContext.Provider> 
-                                 only,
-    I also
-            <IconContext.Provider value={{ color: "#0080A9", className: "fa-1x" }}>
-                <FaHeart />
-            </IconContext.Provider> 
-     These Things
+      <div className="why-text"> I Don't  Like
+              <IconContext.Provider value={{ color: "#ee354b", className: "fa-1x" }}>
+                  <FaLaptop />
+              </IconContext.Provider> 
+                                  only,    I also                                
+              <IconContext.Provider value={{ color: "#0080A9", className: "fa-1x" }}>
+                  <FaHeart />
+              </IconContext.Provider> 
+      These Things
+      </div>
+      <div className="social svg football">
+        <h3>Football   
+                  <FaFutbol />
+        </h3>  
+        <h4>Though I don't play, I am one hell of a fan. And a big sucker for Chelsea Fc</h4>
+      <FootballSvg />
     </div>
-    <div className="social svg football">
-      <h3>Football   
-                <FaFutbol />
-      </h3>  
-      <h4>Though I don't play, I am one hell of a fan. And a big sucker for Chelsea Fc</h4>
-     <FootballSvg />
-  </div>
-  <div className="social svg music">
-      <h3>Music  <FaMusic /></h3> 
-      <h4>From classNameics  to Rock 'n Roll, Down To Modern Pop, Name it I listen to it. <i className="fa fa-chelsea"></i></h4>
-      <MusicSvg />
-  </div>
-    <div className="social svg social-media">
-        <h3>Social Media <FaGithub/></h3>
-        <h4>And When i get a free time, i do love to browse the net.</h4>
-        <SocialMediaSvg />
-     </div>
-  <div className="social svg reading">
-      <h3>Reading <FaBook /> </h3>
-        <h4 className="reading-text">Say No More</h4>
-        <ReadSvg />
-  </div>
-
+    <div className="social svg music">
+        <h3>Music  <FaMusic /></h3> 
+        <h4>From classNameics  to Rock 'n Roll, Down To Modern Pop, Name it I listen to it. <i className="fa fa-chelsea"></i></h4>
+        <MusicSvg />
+    </div>
+      <div className="social svg social-media">
+          <h3>Social Media <FaGithub/></h3>
+          <h4>And When i get a free time, i do love to browse the net.</h4>
+          <SocialMediaSvg />
+      </div>
+    <div className="social svg reading">
+        <h3>Reading <FaBook /> </h3>
+          <h4 className="reading-text">Say No More</h4>
+          <ReadSvg />
+    </div>
 </div>
  )
 }
